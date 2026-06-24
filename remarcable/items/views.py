3fe826cd598701ad114d0ee2a910f176766f1aeb
Josh Martinez <77525064+jshmartin@users.noreply.github.com>
2026-06-24 15:44:21 -0700

    Create Django Template

--- remarcable/items/views.py
+++ remarcable/items/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
-# Create your views here.
+def items(request):
+  template = loader.get_template('index.html')
+  return HttpResponse(template.render())
